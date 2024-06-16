b6436ea29c6fc6a71c109533289c32dc183366e5
picalloj17 <picalloj17@gmail.com>
2024-06-15 22:00:53 -0400

    more stuff trying

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode basename = {process.env.PUBLIC_URL}>
-    <App />
-  </React.StrictMode>
-);
 
-reportWebVitals();
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
